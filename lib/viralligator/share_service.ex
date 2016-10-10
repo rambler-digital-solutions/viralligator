58b6b37bfe86d9f5b1132c0a92253d0272ac71fd
Antonov Alexey <a.antonov@rambler-co.ru>
2016-10-10 21:02:28 +0300

    update

--- lib/viralligator/share_service.ex
+++ lib/viralligator/share_service.ex
@@ -1,23 +1,7 @@
 defmodule Viralligator.ShareService do
   def shares(url)  do
-    [:ok, :vk, :fb, :gplus]
-    |> Enum.map(fn x -> { x, count(url, x) } end)
+    [Ok, Vk, Fb, GPlus]
+    |> Enum.map(fn x -> { x, x.get(url).body } end)
     |> Enum.into(%{})
-  end
-
-  def count(url, :ok) do
-    Ok.get(url).body
-  end
-
-  def count(url, :vk) do
-    Vk.get(url).body
-  end
-
-  def count(url, :fb) do
-    Fb.get(url).body
-  end
-
-  def count(url, :gplus) do
-    GPlus.get(url).body
   end
 end

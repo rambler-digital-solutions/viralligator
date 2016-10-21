c258e2ad3b4c2dfb58c0fd52b745b36188100b71
Antonov Alexey <a.antonov@rambler-co.ru>
2016-10-21 12:41:00 +0300

    list share services from folder

--- lib/viralligator/share_service.ex
+++ lib/viralligator/share_service.ex
@@ -4,8 +4,10 @@ defmodule Viralligator.ShareService do
   """
   alias Viralligator.Models.Share
 
+  @folder_for_socials "lib/viralligator/share_service"
+
   def shares(url)  do
-    [:ok, :vk, :fb, :gplus]
+    list_services
     |> Enum.map(fn x -> %Share{social: to_string(x), count: call(x, url)} end)
   end
 
@@ -16,5 +18,13 @@ defmodule Viralligator.ShareService do
       :fb -> Fb.get(url, cache: true).body
       :gplus -> GPlus.get(url, cache: true).body
     end
+  end
+
+  defp list_services do
+    @folder_for_socials
+    |> Path.join("*.ex")
+    |> Path.wildcard
+    |> Enum.map(fn item -> Path.basename(item, ".ex") end)
+    |> Enum.map(&String.to_atom/1)
   end
 end
